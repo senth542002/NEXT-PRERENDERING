21b640ba69c6147060c649a8593865110e0b8632
Senthil Kumar <vsenth17@ford.com>
2021-11-25 22:53:34 +0530

    Stale data issue

---
+++ pages/products/index.js
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+
+function ProductList ({ products }) {
+
+    return (
+        <> 
+            <h1>List of Products</h1>
+            {
+                products.map(product => {
+                    return (
+                        <div key={product.id}>
+                            <Link href={`products/${product.id}`} passHref>
+                            <h2> {product.id} - {product.title} - {product.price}</h2>
+                            </Link>
+                        <hr/>
+                    </div>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+export default ProductList
+
+export async function getStaticProps() {
+    const response = await fetch('http://localhost:4000/products')
+    const data = await response.json()
+  //  console.log(data)
+    return {
+        props: {
+            products: data,
+        }
+    }
+}

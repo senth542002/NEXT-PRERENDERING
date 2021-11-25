cdf9e72b0d42077dadb4b1d5f840b60d3f102c50
Senthil Kumar <vsenth17@ford.com>
2021-11-25 17:35:27 +0530

    Fetching Paths for getStaticPaths Dyamically

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -24,18 +24,20 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+
+    const paths = data.map(post => {
+        return {
+            params: {
+                postId: `${post.id}`
+            }
+        }
+    })
+
     return {
-        paths: [
-        {
-            params: { postId: '1'},
-        },
-        {
-            params: { postId: '2'},
-        },
-        {
-            params: { postId: '3'},
-        },
-    ],
+    paths,
     fallback: false
     }
 }

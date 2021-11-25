1e830d01f0a5585a654b0dada6373eb8527a1cde
Senthil Kumar V <vsenth17@ford.com>
2021-11-25 13:07:44 +0530

    Static Site Gegeration (SSG) - with dynamic parameters

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Link from 'next/link'
 function Home() {
   return (<>
   <h1>Next JS pre-rendering</h1> <br/>
-  <Link href='/users'>Users</Link>
+  <Link href='/users'><a>Users</a></Link><br/>
+  <Link href='/posts'><a>Posts</a></Link>
   </>)
 }
 
